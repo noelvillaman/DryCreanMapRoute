b171be52f4878253b427c1c1146d85f6b30d8c6a
noelvillaman <namalliv2@hotmail.com>
2022-06-01 08:44:23 -0500

    Part of first commit

--- app/src/main/java/com/software/namalliv/drycreanmaproute/ui/MapsFragment.kt
+++ app/src/main/java/com/software/namalliv/drycreanmaproute/ui/MapsFragment.kt
@@ -3,23 +3,47 @@ package com.software.namalliv.drycreanmaproute.ui
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import com.software.namalliv.drycreanmaproute.R
+import java.lang.Exception
 
 class MapsFragment : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val orlandoFl = LatLng(28.36474806416343, -81.30928845509652)
+        googleMap.addMarker(MarkerOptions().position(orlandoFl).title("Marker in Orlando"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(orlandoFl, 13f))
+        googleMap.uiSettings.apply {
+            isTiltGesturesEnabled = true
+        }
+        changeMapStyle(googleMap)
+    }
+
+    private fun changeMapStyle(googleMap: GoogleMap) {
+        try {
+            val success = googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireContext(),
+                    R.raw.map_style
+                )
+            )
+            if (!success){
+                Log.d("TAG", "Failed to add style")
+            }
+        } catch (e: Exception){
+            Log.d("TAG", e.toString())
+        }
     }
 
     override fun onCreateView(
